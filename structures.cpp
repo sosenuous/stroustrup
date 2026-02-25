f4e3d9b6803b507d3cc19fb07e4d21b60b2455c6
Ondrej Rypacek <ondrej.rypacek@gmail.com>
2026-02-25 18:17:00 +0000

    references

--- structures.cpp
+++ structures.cpp
@@ -1,77 +1,68 @@
 #include <iostream>
 
 
-struct Vector {
-  double* elem; // pointer to elements
-  int sz;       // number of elements
-};
 
-Vector v;
 
-void vector_init(Vector &v , int s) // initialize a Vector
-{ 
-  v.elem = new double[s];
-  v.sz = s;
+
+
+
+
+
+
+void print(int a[], int size)
+{
+
+  for (int i = 0 ; i < size ; i++) {
+    std::cout << a[i] << '\n';
+  }
+
+
+  // print them to cou 
 }
+
+retretret
+
+void swap(int *x , int *y)
+{
+  int z = * y;
+  *y = *x; 
+  *x = z;
+
+  x ++;
+  *y = x [108];
+}
+
+void swap2(const int &x , const int &y) {
+  int z = x;
+  x= y;
+  y = z;
+
+}
+
+
+
 
 using namespace std;
 
-void print(int v[] , int s) {
-  for (auto i = 0 ; i < s ; i++) 
-  {
-    cout << v[i] << "\n";
-  }
-}
-
-// excercise :  the following
-// void print(int *v , int s) ..
-// void print(int v[], int s) ..
-// void print(int )
-
-
-void print()
-{
-  int v1[10] = { 0,1,2,3,4,5,6,7,8,9};
-
-  print(v1,10);
-
-}
-void print(const vector<int> & p){
-
-  for (auto c : p) {
-    cout << c << ',';
-  }
-  cout << '\n';
-
-}
-
-
-
 int main() 
 {
-  print();
-  int v[] = {1,2,3,4,3,2,1};
-  print(v, 7);
-  
-  // zero terminated strings
-  
-  for (const char *p = "Hello World!"; *p ; p++) {
-    cout << *p ;
-  }
-  cout << '\n';
-
-  string hello = "Hello, Universe!"; 
-  cout << hello << '\n';
+  // define an array of 10 integers
+  int v[] = {1,2,3,4,5,6,8,9,10};
 
 
-  constexpr auto pi  = (float)3141593 / 1000000;
-  cout << pi << '\n' ;
+  print( v , 10);
+
+  int a = 1, b = 10;
+  cout << "a == " << a <<", b == " << b << '\n';
+  swap(&a, &b);
+  cout << "a == " << a <<", b == " << b << '\n';
+  swap2(a,b);
+
+  cout << "a == " << a <<", b == " << b << '\n';
 
 
-  print(vector<int>({1,2,4,5,7,8,24}));
+  // ( a == 10 && b = 1);
 
-  char *p = 0;
-  
 
   return 0;
 }

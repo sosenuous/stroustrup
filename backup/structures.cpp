3a8b99a11b153443cede9610bcfeb5653938761f
quemuyricouk <quemuyrico@national.shitposting.agency>
2026-04-01 18:36:00 +0100

    yes
    cmon :(

--- backup/structures.cpp
+++ backup/structures.cpp
@@ -1,92 +1,52 @@
 #include <iostream>
+#include "structures.h"
+#include <assert.h>
 
 
-void print(int a[], int size)
-{
-  for (int i = 0 ; i < size ; i++) {
-    std::cout << a[i] << '\n';
-  }
 
-  // print them to cou 
+
+// implement operator+ that adds two vectors component-wise 
+// c = a + b ; // where a and b are Vectors
+Vector operator+(const Vector &a , const Vector &b);
+
+Vector::Vector(int n) : Container(n), data (new int[n]) { 
+    assert(n >= 0);
+    for (int i = 0; i < size; i++) data[i] = 0; 
 }
 
-
-void swap(int & x , int & y)
-{
-  int z = y;
-  y = x; 
-  x = z;
-}
-
-
-
-
-class Vector {
-
-  int len;
-  int * data;
-
-  public:
-  Vector(int n) : len(n), data (new int[n]) { 
-    for (int i = 0; i < len; i++) data[i] = 0; 
-  }
-
-  Vector(int n , const int  a[] ) : len (n), data(new int[n]) 
+Vector::Vector(int n , const int  a[] ) : Container(n) , data(new int[n]) 
   { 
     for (auto i = 0; i < n; ++i) data[i] = a[i];
   }
 
-
-  ~Vector() {
-    delete [] data;
+Vector::~Vector() {
+    std::cout << "~Vector";
+    if (data) delete [] data;
   }
 
-  int &operator[](int i) {
-    return data[i];
+  // implement operator+ that adds two vectors component-wise 
+  // c = a + b ; // where a and b are Vectors
+  Vector operator+(const Vector &a , const Vector &b) { 
+    if (a.length() != b.length()) { 
+        throw std::length_error("vector should obvs be the same length duh");
+    }
+    
+    Vector result(a.length()); 
+    
+    for (int i = 0; i < a.length(); i++) { 
+      result[i] = a[i] + b[i]; 
+    }
+    
+    return result; 
+   }
+
+/*
+Vector::Vector(std::initializer_list<int> lst) : len(lst.size()), data(new int [lst.size()])
+
+
+  {
+    std::copy(lst.begin(), lst.end(), data);
   }
-
-  int length() { return len; }
-
-
-  protected:
- 
- 
-};
-
-/* 
-[ ][ ]  --->   [ ][ ][ ][ ][ ][ ][ ] 
 
 
 */
-
-
-
-using namespace std;
-
-
- 
-
-
-
-int main() 
-{
-  auto x = 1;
-  auto y = 0;
-
-  Vector v(5); 
-  v[0] = 42;  // v.operator[](0) = 42;
-  cout << v[0]; // v.operator[](0);
-
-  swap(x, y);
-
-  cout << " x == " << x << " , y == " << y << "\n";
-
-  int a[]= {1,2,3,4,5};
-  Vector w(5, a);
-  for (auto i = 0 ; i< 5; ++i) cout << w[i] << "," ;
-  cout << "\n";
-
-  
-
-  return 0;
-} ///  v.~Vector() ; w.~Vector();
